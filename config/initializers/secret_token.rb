15912f82134de3ce1f68837a714e6c48afc81d2a
Steve Hudson <strayhud@gmail.com>
2014-03-18 00:32:24 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FarkleServer::Application.config.secret_key_base = '57040a92b95f90bcc592896dd78db5f2170aa9ada34e4f6dbc01849148aac46a50a996d210d42e7c904de80368cafe68408f2da24afc4241bf9bf8792bc20569'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FarkleServer::Application.config.session_store :cookie_store, key: '_farkle_server_session'
